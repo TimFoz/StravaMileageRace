ab0ec969880084ba932fbebfbbfb6065c04f99c8
Tim Fozz <tim.fozzard@gmail.com>
2023-06-18 22:42:52 +0100

    graph design changes, adding pacer

--- public/scripts/chart.js
+++ public/scripts/chart.js
@@ -34,22 +34,42 @@ const img1 = new Image();
 img1.src = 'tim.png';
 const img2 = new Image(); 
 img2.src = 'jack.png';
-const imgHeight = 76;
-const imgWidth = 51;
+const img3 = new Image(); 
+img3.src = 'kippy.png';
+const img1Height = 76;
+const img1Width = 51;
+const img2Height = 76;
+const img2Width = 51;
+const img3Height = 76;
+const img3Width = 61;
 
-const datapoints = [timMiles, jackMiles];
+
+
+const getDaysRatio = () => {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const startOfYear = new Date(currentYear, 0, 1);
+  const elapsedMilliseconds = currentDate - startOfYear;
+  const totalMilliseconds = new Date(currentYear + 1, 0, 1) - startOfYear;
+  const ratio = elapsedMilliseconds / totalMilliseconds;
+  return ratio;
+};
+const pace = (3000 * getDaysRatio()).toFixed(1);
+const datapoints = [timMiles, jackMiles, pace];
 
 const data = {
-  labels: ['Tim', 'Jack' ],
+  labels: ['Tim', 'Jack', 'pacer' ],
   datasets: [{
     label: 'YTD mileage',
     data: datapoints,
     barThickness: 20,
     backgroundColor: [
       'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
       'rgba(255, 159, 64, 0.2)'
     ],
     borderColor: [
+      '#FC4C02',
       '#FC4C02',
       '#FC4C02'
     ],
@@ -66,8 +86,9 @@ const barAvatar = {
       scales: {x, y} } = chart;
     ctx.save()
 
-    ctx.drawImage(img1, x.getPixelForValue(datapoints[0]) - imgWidth/2, y.getPixelForValue(0) - imgHeight/2, imgWidth, imgHeight)
-    ctx.drawImage(img2, x.getPixelForValue(datapoints[1]) - imgWidth/2, y.getPixelForValue(1) - imgHeight/2, imgWidth, imgHeight)
+    ctx.drawImage(img1, x.getPixelForValue(0) - img1Width/2, y.getPixelForValue(datapoints[0]) - img1Height/2, img1Width, img1Height)
+    ctx.drawImage(img2, x.getPixelForValue(1) - img2Width/2, y.getPixelForValue(datapoints[1]) - img2Height/2, img2Width, img2Height)
+    ctx.drawImage(img3, x.getPixelForValue(2) - img3Width/2, y.getPixelForValue(datapoints[2]) - img3Height/2, img3Width, img3Height)
   }
 }
 
@@ -76,7 +97,6 @@ const config = {
   type: 'bar',
   data,
   options: {
-    indexAxis: 'y',
     plugins: {
       legend: {
         display: false
@@ -93,11 +113,12 @@ const config = {
         }
     },
     scales: {
-      x: {
+      y: {
         beginAtZero: true,
         max: 3000
       }
-    }
+    },
+    maintainAspectRatio: false,
   },
   plugins: [barAvatar]
 };
